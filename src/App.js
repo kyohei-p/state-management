ca279ee352afcb86088ba9a49d50a7d89a7e788b
kyohei-p <107188352+kyohei-p@users.noreply.github.com>
2023-09-23 07:03:10 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { UserProvider } from "./providers/UserProvider";
+import { Router } from "./router/Router";
+import { RecoilRoot } from "recoil";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <RecoilRoot>
+      <UserProvider>
+        <Router />
+      </UserProvider>
+    </RecoilRoot>
+  );
+}
